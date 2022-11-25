48d431bc937916e96466dfdacbba42ff70b63388
aplacaba <alacaba@fastmail.com>
2022-11-26 01:29:31 +0800

    parse endpoint response body

--- src/apex_bot/api.clj
+++ src/apex_bot/api.clj
@@ -1,16 +1,19 @@
 (ns apex-bot.api
   (:require [clj-http.client :as client]
-            [environ.core    :refer [env]]))
+            [environ.core    :refer [env]]
+            [clojure.data.json :as json]))
 
 (def url (env :apex-api))
 (def api-key (env :apex-api-key))
 
-(defn map-rotation
-  []
-  (client/get
-   (str url "/maprotation?auth=" api-key)))
+(defn proc-request
+  [endpoint]
+  (-> endpoint client/get :body json/read-str))
 
-(defn crafting-rotation
-  []
-  (client/get
-   (str url "/crafting?auth=" api-key)))
+(defn map-rotation []
+  (let [endpoint (str url "/maprotation?auth=" api-key)]
+    (proc-request endpoint)))
+
+(defn crafting-rotation []
+  (let [endpoint (str url "/crafting?auth=" api-key)]
+    (proc-request endpoint)))
